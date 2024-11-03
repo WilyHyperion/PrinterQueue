74f241cf4becbe3900ab3b68bfa8eb444aab77e4
Cjgoal <Colinjenn123@gmail.com>
2024-11-02 23:38:22 -0700

    More  Ui Stlying

--- amadorprintmanager/pages/logged/browse.tsx
+++ amadorprintmanager/pages/logged/browse.tsx
@@ -11,15 +11,48 @@ export default function BrowseJobs() {
             console.log(t)
         })
     }, [])
-    return(<div className ="w-full min-h-screen flex items-center justify-center">
-        <ol className="w-1/2">
+    return(
+    <div className="flex flex-col">
+        <div className="bg-white w-full h-15 text-black text-3xl p-3 flex justify-center items-center ">
+            <text>Order Submissions</text>
+        </div>
+        <div className ="w-full h-screen flex  flex-col justify-center bg-gradient-to-r  from-indigo-900 via-purple-800 to-purple-900 ">
+        <div className="bg-purple-950 w-auto h-auto p-5 m-5 rounded-3xl flex flex-row">
+            <text className="mx-5 ml-5">Hello</text>
+            <text className="mx-5 ml-5">Hello</text>
+            <text className="mx-5 ml-5">Hello</text>
+        </div> 
+        <ol className="w-full h-full">
             {jobs.map((job) => {
-                return <li onClick = {
-                    (e) => {
-                        window.location.href = `/logged/job/${job.id}`
-                    }
-                } key={job.id}>{job.name} by {job.user.email} | {job.status}  </li>
+                return  <li 
+                key={job.id} 
+                className="p-2 m-4 bg-white rounded-lg shadow-md cursor-pointer hover:bg-gray-200 transition-colors"
+                onClick={() => {
+                  window.location.href = `/logged/job/${job.id}`;
+                }}
+              >
+                <div className="flex flex-col md:flex-row md:justify-between md:items-center ">
+                  <div>
+                    <h3 className="text-lg text-black">{job.name}</h3>
+                    <p className="text-black">Submitted by {job.user.email}</p>
+                  </div>
+                  <div className="mt-2 md:mt-0">
+                    <span 
+                      className={`px-3 py-1 rounded-full text-sm font-medium ${
+                        job.status === 'completed' 
+                          ? 'bg-green-200 text-green-800' 
+                          : job.status === 'In Progre' 
+                          ? 'bg-yellow-200 text-black' 
+                          : 'bg-red-200 text-red-800'
+                      }`}
+                    >
+                      {job.status}
+                    </span>
+                  </div>
+                </div>
+              </li>
             })}
         </ol>
+    </div>
     </div>)
 }
