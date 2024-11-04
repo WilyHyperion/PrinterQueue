99860f0f78f991cc88da5ca9687bf1aa4c7475ce
WilyHyperion <ns9316@pleasantonusd.net>
2024-11-04 14:54:50 -0800

    backend for update document

---
+++ amadorprintmanager/pages/api/updatestatus.ts
@@ -0,0 +1,44 @@
+
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Job } from "@/types/types";
+import { auth } from "@/auth";
+import db from "@/lib/db";
+import { ObjectId } from "mongodb";
+
+
+export  default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+    let user = await auth(req, res)
+    if(!user){
+        console.log("failed")
+        return
+    }
+    if(!user?.user){
+        console.log("failed")
+        return
+    }
+    if(user?.user.role !== "teacher") {
+        console.log("failede")
+        return 
+    }
+    console.log(req.body)
+    let job = await db.collection("Jobs").updateOne({
+        _id: new ObjectId(req.body.id)   
+    }, {
+        $set: {
+            status: 
+                 req.body.status   
+        }
+    })
+    console.log(job)
+    console.log("updated")
+    res.status(200).json({
+        message: "updated"
+    })
+
+
+}
