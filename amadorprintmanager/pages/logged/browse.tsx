4b96c27fe128660fd1b987a7181cb77dbfcb18a5
WilyHyperion <ns9316@pleasantonusd.net>
2024-11-13 21:07:33 -0800

    funky filter movement

--- amadorprintmanager/pages/logged/browse.tsx
+++ amadorprintmanager/pages/logged/browse.tsx
@@ -3,7 +3,12 @@ import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import JobChart from "@/components/jobChart";
 import Image from "next/image";
-import { equalFilters, Filter, FilterTypes, getInputElement } from "@/types/Filters";
+import {
+  equalFilters,
+  Filter,
+  FilterTypes,
+  getInputElement,
+} from "@/types/Filters";
 import { literalToPrettyName } from "@/types/Constants";
 export default function BrowseJobs() {
   const [jobs, setJobs] = useState([] as Job[]);
@@ -12,16 +17,22 @@ export default function BrowseJobs() {
   const [filterMaking, setFilterMaking] = useState(null as Filter | null);
   const [selectedCollumn, setSelectedCollumn] = useState("Date");
   const [inputs, setInputs] = useState([] as any[]);
+  const [drag, setDrag] = useState({
+    x: 0,
+    y: 0,
+    dragging: false,
+  });
   useEffect(() => {
     fetch("/api/getjobs").then(async (res) => {
       let t = await res.json();
       setJobs(t);
-      console.log(t);
     });
   }, []);
   return (
     <div className="flex flex-col ">
-      <div className="w-full h-full  bg-gradient-to-r  from-indigo-900 via-purple-800 to-purple-900 ">
+      <div
+        className={`w-full h-full  bg-gradient-to-r  from-indigo-900 via-purple-800 to-purple-900  `}
+      >
         <Image
           className=" absolute left-5 top-5 cursor-pointer    "
           alt="Back"
@@ -45,7 +56,37 @@ export default function BrowseJobs() {
             Add Filter
           </button>{" "}
           {open && (
-            <div className={`absolute top-[5%] p-[3%] border-gray-300 border-2 bg-gray-50 w-1/4 rounded-xl flex flex-col cursor-move `}  >
+            <div
+              className={`absolute p-[3%] border-gray-300 border-2 bg-gray-50 w-1/4 rounded-xl flex flex-col cursor-move text-black `}
+              style={{
+                top: drag.y,
+                left: drag.x,
+              }}
+              onMouseDown={(e) => {
+                document.onmouseup = (e) => {
+                  setDrag({
+                    x: e.pageX,
+                    y: e.pageY,
+                    dragging: false,
+                  });
+                  document.onmousemove = null;
+                };
+                document.onmousemove = (e) => {
+                  let offset = e.target as HTMLElement;
+                  setDrag({
+                    x: e.clientX - offset.offsetTop,
+                    y: e.clientY - offset.offsetHeight,
+                    dragging: true,
+                  });
+                };
+                let offset = e.target as HTMLElement;
+                setDrag({
+                  x: e.clientX - offset.offsetTop,
+                  y: e.clientY - offset.offsetHeight,
+                  dragging: true,
+                });
+              }}
+            >
               <Image
                 src={"/close.svg"}
                 alt="X"
@@ -63,10 +104,12 @@ export default function BrowseJobs() {
                   setSelectedCollumn(e.target.value);
                 }}
               >
-                <option selected disabled>--Choose A Collumn--</option>
+                <option selected disabled>
+                  --Choose A Collumn--
+                </option>
                 {Object.keys(literalToPrettyName).map((key) => {
                   return (
-                    <option value={key} className="bg-gray-500">
+                    <option value={key} className="">
                       {literalToPrettyName[key]}
                     </option>
                   );
@@ -84,24 +127,34 @@ export default function BrowseJobs() {
                     console.log(FilterType);
                     setFilterMaking(new FilterType([] as any[], ""));
                     let inputs = [] as any[];
-                    for (let i = 0; i < new FilterType([], "").inputTypes.length; i++) {
-                      inputs.push("")
+                    for (
+                      let i = 0;
+                      i < new FilterType([], "").inputTypes.length;
+                      i++
+                    ) {
+                      inputs.push("");
                     }
                     setInputs(inputs);
                   }
                 }}
               >
-                <option selected disabled>--Choose A Filter Type--</option>
+                <option selected disabled>
+                  --Choose A Filter Type--
+                </option>
                 {FilterTypes.map((FilterType) => {
                   if (
-                    (new FilterType([] as any[], "").vaildCatagoies?.includes(
+                    new FilterType([] as any[], "").vaildCatagoies?.includes(
                       selectedCollumn
-                    ) || (new FilterType([] as any[], "").vaildCatagoies?.includes(
-                      "*")
-                      ))
+                    ) ||
+                    new FilterType([] as any[], "").vaildCatagoies?.includes(
+                      "*"
+                    )
                   ) {
                     return (
-                      <option value={FilterType.name} className="p-2 bg-[#e5e7eb] text-black">
+                      <option
+                        value={FilterType.name}
+                        className="p-2  text-black"
+                      >
                         {FilterType.name.replace(/([A-Z])/g, " $1")}
                       </option>
                     );
@@ -111,26 +164,35 @@ export default function BrowseJobs() {
               {filterMaking &&
                 filterMaking.inputTypes.map((inputType, i) => {
                   let type = inputType.name;
-                  return <div className="flex flex-row justify-between items-center">
-                    <strong className="w-[20%]">{filterMaking.inputLabels[i]}</strong>
-                    {getInputElement(type, (e) => {
-                      let newInputs = inputs;
-                      newInputs[i] = e.target.value;
-                      setInputs(newInputs);
-                    })
-                    }</div>;
+                  return (
+                    <div className="flex flex-row justify-between items-center">
+                      <strong className="w-[20%]">
+                        {filterMaking.inputLabels[i]}
+                      </strong>
+                      {getInputElement(type, (e) => {
+                        let newInputs = inputs;
+                        newInputs[i] = e.target.value;
+                        setInputs(newInputs);
+                      })}
+                    </div>
+                  );
                 })}
-              <button className="bg-[#e5e7eb] mt-5 p-2 text-black" onClick={() => {
-                if (filterMaking) {
-                  if (filterMaking.inputs.includes("")) {
-                    return
-                  }
-                  filterMaking.inputs = inputs
-                  filterMaking.catagory = selectedCollumn
+              <button
+                className="bg-[#e5e7eb] mt-5 p-2 text-black"
+                onClick={() => {
+                  if (filterMaking) {
+                    if (filterMaking.inputs.includes("")) {
+                      return;
+                    }
+                    filterMaking.inputs = inputs;
+                    filterMaking.catagory = selectedCollumn;
 
-                  setFilter([...filter, filterMaking])
-                }
-              }}>Add Filter</button>
+                    setFilter([...filter, filterMaking]);
+                  }
+                }}
+              >
+                Add Filter
+              </button>
             </div>
           )}
         </div>
@@ -138,7 +200,6 @@ export default function BrowseJobs() {
           {filter.map((obj) => {
             return (
               <div>
-
                 {obj.catagory + " "}
                 {obj.name + " "}
                 {obj.inputs.map((input) => {
@@ -151,19 +212,18 @@ export default function BrowseJobs() {
                   src="/close.svg"
                   className=""
                   onClick={(e) => {
-                    console.log(filter, "old")
+                    console.log(filter, "old");
                     for (let f of filter) {
                       if (equalFilters(f, obj)) {
-                        let newFilter = filter
-                        newFilter.splice(filter.indexOf(f), 1)
-                        console.log(newFilter, "new")
-                        setFilter(newFilter)
-                        break
+                        let newFilter = filter;
+                        newFilter.splice(filter.indexOf(f), 1);
+                        console.log(newFilter, "new");
+                        setFilter(newFilter);
+                        break;
                       }
                     }
                   }}
-                >
-                </Image>
+                ></Image>
               </div>
             );
           })}
