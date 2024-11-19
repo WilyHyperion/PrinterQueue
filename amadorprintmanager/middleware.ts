393ac8ab8a5513662da057dfced948c73631e9ca
WilyHyperion <ns9316@pleasantonusd.net>
2024-11-19 15:03:46 -0800

    Last time I swear

--- amadorprintmanager/middleware.ts
+++ amadorprintmanager/middleware.ts
@@ -6,7 +6,7 @@ export async function middleware(request: NextRequest) {
     const cookies = request.cookies;
     for(let c of cookies){
       console.log(c)
-      if(c[0].includes('token')){
+      if(c[0].includes('session-token')){
         return NextResponse.next()
       }
     }
