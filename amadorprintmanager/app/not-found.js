539dfa52b4f0c9741e6202e9d010f0480a8a6580
WilyHyperion <ns9316@pleasantonusd.net>
2024-11-19 14:39:34 -0800

    hopefully fix build error

--- amadorprintmanager/app/not-found.js
+++
@@ -1,9 +0,0 @@
-export default function Custom404() {
-  return <html>
-    <body>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you are looking for does not exist.</p>
-      <a href = "/">Return to homepage</a>
-    </body>
-  </html>;
-}
